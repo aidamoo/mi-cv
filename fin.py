6376777aa80e0350be3775fd45d5832caec16aa5
Aida <aida.amoedo.port@gmail.com>
2024-03-20 21:36:11 +0100

    ultimos commit

--- fin.py
+++ fin.py
@@ -4,7 +4,7 @@ from PIL import Image
 def fin():
     st.title("Gracias!")
     image6 = Image.open(
-        "imagenes/finnn.PNG")
+        "imagenes/finnn.png")
     st.image(image=image6, width=450)
 
 if __name__ == "__main__":
