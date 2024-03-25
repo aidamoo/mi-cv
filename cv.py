493b9496e849121064da135a532a06bf25120ff8
Aida <aida.amoedo.port@gmail.com>
2024-03-25 15:56:29 +0100

    ultimos commit

--- cv.py
+++ cv.py
@@ -5,7 +5,7 @@ def cv():
     st.title("Mi CV")
 
     image = Image.open(
-        "imagenes/Data Science & Inteligencia Artificial - HACK A BOSS (Cursando).png")
+        "imagenes/CV AIDA.png")
     st.image(image=image, width=800)
 
 if __name__ == "__main__":
